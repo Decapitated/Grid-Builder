cdd7f4899cf8a2422252f6a995b337f05ee47d4c
Decapitated <brettzkyjo10@gmail.com>
2023-01-28 02:38:28 -0600

    Character controller changes.

--- Assets/Src/Scripts/PlayerController.cs
+++ Assets/Src/Scripts/PlayerController.cs
@@ -7,25 +7,45 @@ public class PlayerController : MonoBehaviour
     public bool firstPerson = true;
     public bool canFly = false;
 
+    public bool IsGrounded { get; private set; } = true;
+
     public float mouseSpeed = 100f;
     public float mouseMultiplier = 10f;
     public float clickLength = 0.333f;
 
+    public float jumpPower = 5f;
     public float movementSpeed = 10f;
+    public float sprintMultiplier = 1.5f;
 
+    CharacterController controller;
     Rigidbody rigidbody;
     public Transform cameraTarget;
     Camera camera;
     Animator animator;
+    Renderer renderer;
     float rotationX = 0f;
 
 
     void Awake()
     {
+        controller = GetComponent<CharacterController>();
         rigidbody = GetComponent<Rigidbody>();
-        rigidbody.sleepThreshold = 0;
+        renderer = GetComponentInChildren<Renderer>();
         camera = GetComponentInChildren<Camera>();
         animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        var velocity = rigidbody.velocity;
+        UpdateAnimations();
+
+        if (!IsGrounded) print("Not grounded.");
+        if (IsGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+            rigidbody.velocity = new(velocity.x, rigidbody.velocity.y, velocity.z);
+        }
     }
 
     void FixedUpdate()
@@ -48,9 +68,28 @@ public class PlayerController : MonoBehaviour
         }
         cameraTarget.transform.localRotation = Quaternion.Euler(-rotationX, 0f, 0f);
 
-        if (canFly && rigidbody.useGravity)        rigidbody.useGravity = false;
-        else if (!canFly && !rigidbody.useGravity) rigidbody.useGravity = true;
+        // Check whether the animation controls the position of the character.
+        if (!animator.applyRootMotion) UpdateMovement();
 
+        print(rigidbody.velocity);
+    }
+
+    void UpdateMovement()
+    {
+        var move = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) move += transform.forward * (!(Input.GetKey(KeyCode.LeftShift)) ? 1f : sprintMultiplier);
+        if (Input.GetKey(KeyCode.S)) move += -transform.forward;
+        if (Input.GetKey(KeyCode.A)) move += -transform.right;
+        if (Input.GetKey(KeyCode.D)) move += transform.right;
+        move *= movementSpeed;
+        rigidbody.velocity = new(move.x, rigidbody.velocity.y, move.z);
+        //rigidbody.AddForce(movementSpeed * Time.deltaTime * move, ForceMode.Force);
+        //controller.Move(movementSpeed * Time.deltaTime * move);
+        //rigidbody.MovePosition(transform.position + (movementSpeed * Time.deltaTime * move));
+    }
+
+    void UpdateAnimations()
+    {
         int moveTypeId = Animator.StringToHash("movementType");
         int walkDirId = Animator.StringToHash("walkDir");
         int jumpId = Animator.StringToHash("jump");
@@ -60,7 +99,7 @@ public class PlayerController : MonoBehaviour
             // Posotive multiplier gives forwards walk.
             animator.SetFloat(walkDirId, 1f);
             if (Input.GetKey(KeyCode.LeftShift))
-                 animator.SetInteger(moveTypeId, 2);
+                animator.SetInteger(moveTypeId, 2);
             else animator.SetInteger(moveTypeId, 1);
         }
         else if (Input.GetKey(KeyCode.S))
@@ -72,9 +111,34 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKey(KeyCode.A)) animator.SetInteger(moveTypeId, 4);
         else if (Input.GetKey(KeyCode.D)) animator.SetInteger(moveTypeId, 3);
         else animator.SetInteger(moveTypeId, 0);
-        if (Input.GetKey(KeyCode.Space)) animator.SetTrigger(jumpId);
-        //if (canFly && Input.GetKey(KeyCode.Space)) keyDir += Vector3.up;
-        //if (canFly && Input.GetKey(KeyCode.LeftControl)) keyDir += Vector3.down;
-        //transform.Translate((Input.GetKey(KeyCode.LeftShift) ? 10f : 1f) * movementSpeed * Time.deltaTime * keyDir, Space.Self);
+        if (IsGrounded && Input.GetKeyDown(KeyCode.Space)) animator.SetTrigger(jumpId);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        var bottom = renderer.bounds.center;
+        bottom.y -= renderer.bounds.extents.y;
+        float minDist = float.PositiveInfinity;
+        float angle = 180f;
+        // Find closest point to bottom.
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            var contact = collision.GetContact(i);
+            var tempDist = Vector3.Distance(contact.point, bottom);
+            if(tempDist < minDist)
+            {
+                minDist = tempDist;
+                // Check how close the contact normal is to our up vector.
+                angle = Vector3.Angle(transform.up, contact.normal);
+            }
+        }
+        // Check if the angle is too steep.
+        if (angle <= 45f) IsGrounded = true;
+        else IsGrounded = false;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        IsGrounded = false;
     }
 }
